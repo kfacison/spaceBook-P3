af243c560bf376d90bd97f89985d6cf74eebdb2b
kfac <kfacison1998@gmail.com>
2023-11-16 13:54:59 -0600

    init commit

---
+++ src/pages/AuthPage/AuthPage.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+import SignUpForm from "../../components/SignUpForm/SignUpForm";
+import LoginForm from '../../components/LoginForm/LoginForm';
+
+export default function AuthPage({setUser}){
+    const [authState, setAuthState ] = useState([true]);
+    return (
+        <main>
+        <h1>AuthPage</h1>
+        <button onClick={() => setAuthState(!authState)}>{authState ? "Login" : "SignUp"}</button>
+        {authState && 
+        <>
+        <h2>SignUp</h2>
+        <SignUpForm setUser={setUser}/>
+        </>
+        }
+        { !authState &&
+        <>
+        <h2>Login</h2>
+        <LoginForm setUser={setUser}/>
+        </>
+        }
+        </main>
+    );
+}
