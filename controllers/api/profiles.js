b5adfa47b452c85cb39b5e4082869f27428170af
caitlincorrigan <caitlinfcorrigan@gmail.com>
2023-11-18 12:36:58 -0800

    stubs up CRUD functions for profiles and posts

--- controllers/api/profiles.js
+++ controllers/api/profiles.js
@@ -1,3 +1,24 @@
-module.exports = {
+const Profile = require('../../models/profile');
 
+module.exports = {
+    getProfile,
+    update,
+    deleteProfile,
+    getAll
+}
+
+async function getAll(req, res) {
+    console.log("Hit getAll controller")
+}
+
+async function deleteProfile(req, res) {
+    console.log("Hit deleteProfile controller")
+}
+
+async function update(req, res) {
+    console.log("Hit update controller")
+}
+
+async function getProfile(req, res) {
+    console.log("Hit getProfile controller")
 }
