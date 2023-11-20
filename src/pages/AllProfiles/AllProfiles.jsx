2eff099f9e96bc38021c01d2639e459bc8eb3cc3
mHaines331 <mhaines9219@gmail.com>
2023-11-20 09:38:44 -0800

    changes

--- src/pages/AllProfiles/AllProfiles.jsx
+++ src/pages/AllProfiles/AllProfiles.jsx
@@ -1,18 +1,16 @@
 import UserAvatar from "../../components/UserAvatar/UserAvatar";
 import * as usersService from "../../utilities/users-service";
 import "./AllProfiles.css";
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 const dummyUserAccounts = [
-  { id: 1, username: 'user1', email: 'user1@example.com' },
-  { id: 2, username: 'user2', email: 'user2@example.com' },
-
+  { id: 1, username: "user1", email: "user1@example.com" },
+  { id: 2, username: "user2", email: "user2@example.com" },
 ];
 
 const AllProfiles = () => {
   const [userAccounts, setUserAccounts] = useState([]);
   useEffect(() => {
-  
     setUserAccounts(dummyUserAccounts);
   }, []);
 
@@ -22,7 +20,8 @@ const AllProfiles = () => {
       <ul>
         {userAccounts.map((user) => (
           <li key={user.id}>
-            <strong>Username:</strong> {user.username}, <strong>Email:</strong> {user.email}
+            <strong>Username:</strong> {user.username}, <strong>Email:</strong>{" "}
+            {user.email}
           </li>
         ))}
       </ul>
@@ -31,65 +30,3 @@ const AllProfiles = () => {
 };
 
 export default AllProfiles;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// export default function AllProfiles() {
-//   //   async function handleCheckToken() {
-//   //     const expDate = await usersService.checkToken();
-//   //     console.log(expDate);
-//   //   }
-
-//   return (
-//     <>
-//       <h1>All Users</h1>
-//       <div id="all-users-container">
-//         {/* will need to extract each USER to be a component */}
-//         <div className="fake-user">Fake user</div>
-//         <div className="fake-user">Fake user</div>
-//         <div className="fake-user">Fake user</div>
-//         <div className="fake-user">Fake user</div>
-//         <div className="fake-user">Fake user</div>
-//         <div className="fake-user">Fake user</div>
-//         <div className="fake-user">Fake user</div>
-//         <div className="fake-user">Fake user</div>
-//         <div className="fake-user">Fake user</div>
-//         <div className="fake-user">Fake user</div>
-//         <div className="fake-user">Fake user</div>
-//         <div className="fake-user">Fake user</div>
-//         <div className="fake-user">Fake user</div>
-//         <div className="fake-user">Fake user</div>
-//         <div className="fake-user">Fake user</div>
-//         <div className="fake-user">Fake user</div>
-//       </div>
-//     </>
-//   );
-// }
