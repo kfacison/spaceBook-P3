40896506f95f898aaeffab53bebed1ad8fb31ebb
mHaines331 <mhaines9219@gmail.com>
2023-11-19 17:14:45 -0800

    style timeline and postbox

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import "./AboutMe.css";
 import { Link } from "react-router-dom";
 
-export default function AboutMe() {
+export default function AboutMe({ profile }) {
   return (
     <>
-      <div id="profile-photo-container">PHOTO</div>
-      <div id="about-me-box">ABOUT ME</div>
+      <div id="profile-photo-container">div</div>
+      <div id="about-me-box">ABOUT ME{profile.favoritePlanet}</div>
       <Link to="/profiles/:id/edit">EDIT PROFILE</Link>
     </>
   );
