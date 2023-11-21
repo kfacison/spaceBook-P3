2614c433f1c2e8c84b5bac36e6913f145c1d5703
mHaines331 <mhaines9219@gmail.com>
2023-11-20 17:39:50 -0800

    update

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import "./AboutMe.css";
 import { Link } from "react-router-dom";
 
-export default function AboutMe({ profile }) {
+export default function AboutMe({ myProfile }) {
   return (
     <>
       <div id="profile-photo-container">div</div>
       <div id="about-me-wrapper">
         <div id="about-me-box">
-          <div className="about-me-cat">Name: (user.name)</div>
+          <div className="about-me-cat">{myProfile[0].username}</div>
           <div className="about-me-cat">Age: (user.age)</div>
           <div className="about-me-cat">
             Favorite Planet: (user.favoritePlanet)
