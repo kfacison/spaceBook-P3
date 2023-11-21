a77ef598b136782ae91f39d8bbae37e0da868903
mHaines331 <mhaines9219@gmail.com>
2023-11-20 21:09:43 -0800

    update

--- src/components/PostComponent/PostComponent.jsx
+++ src/components/PostComponent/PostComponent.jsx
@@ -20,13 +20,13 @@ export default function PostComponent() {
           <ul id="old-posts-list">
             {" "}
             {/*ADD FUNCTION TO ITERATE OVER OLD POSTS AND EXTRACT THIS TO COMPONENT*/}
-            <li>test</li>
+            <li className="div-text">test</li>
             <hr></hr>
-            <li>test</li>
+            <li className="div-text">test</li>
             <hr></hr>
-            <li>test</li>
+            <li className="div-text">test</li>
             <hr></hr>
-            <li>test</li>
+            <li className="div-text">test</li>
             <hr></hr>
           </ul>
         </div>
