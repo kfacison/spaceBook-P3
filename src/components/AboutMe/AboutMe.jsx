d9447075d5303ede45a8aa7ebaee6dc1a40994b7
kfac <kfacison1998@gmail.com>
2023-11-20 18:26:03 -0600

    populated about me with myProfile info

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -2,18 +2,18 @@ import React from "react";
 import "./AboutMe.css";
 import { Link } from "react-router-dom";
 
-export default function AboutMe({ profile }) {
+export default function AboutMe({ myProfile }) {
   return (
     <>
       <div id="profile-photo-container">div</div>
       <div id="about-me-wrapper">
         <div id="about-me-box">
-          <div className="about-me-cat">Name: (user.name)</div>
-          <div className="about-me-cat">Age: (user.age)</div>
-          <div className="about-me-cat">
-            Favorite Planet: (user.favoritePlanet)
-          </div>
+          <div className="about-me-cat">Name: {myProfile[0].username}</div>
+          <div className="about-me-cat">Species: {myProfile[0].species}</div>
+          <div className="about-me-cat">Favorite Planet: {myProfile[0].favPlanet}</div>
+          <div className="about-me-cat">Bio: {myProfile[0].bio}</div>
         </div>
+        {/* if the user have the same id them they can see the edit button {myProfile[0].user} */}
         <Link to="/profiles/:id/edit" id="edit-profile-link">
           <img
             src="https://i.ibb.co/ysycYrk/edit-square-FILL0-wght400-GRAD0-opsz24.png"
