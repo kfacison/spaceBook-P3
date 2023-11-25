0ee5781859ec9b6c05a27bfd6d7d59f48f2e70fb
mHaines331 <mhaines9219@gmail.com>
2023-11-24 18:59:27 -0800

    add friends functionality accomodates multiple friends, does not allow same person to be added, does not allow user to add themselves

--- src/pages/AllProfiles/AllProfiles.jsx
+++ src/pages/AllProfiles/AllProfiles.jsx
@@ -5,7 +5,7 @@ import { update } from "../../utilities/profiles-api";
 import { getAll } from "../../utilities/profiles-api";
 import { Link } from "react-router-dom";
 
-const AllProfiles = ({myProfile, setMyProfile}) => {
+const AllProfiles = ({ myProfile, setMyProfile }) => {
   const [allProfiles, setAllProfiles] = useState([]);
 
   useEffect(function () {
@@ -16,16 +16,47 @@ const AllProfiles = ({myProfile, setMyProfile}) => {
     }
     getAllProfiles();
   }, []);
-  
- async function handleAddFriend(p){
-const newFriend = {
-  _id : myProfile._id, friends: p
-}
-console.log(newFriend)
-const sendFriend = await update(newFriend)
-setMyProfile(sendFriend);
+
+  //  async function handleAddFriend(p){
+  // const newFriend = {
+  //   _id : myProfile._id, friends: p
+  // }
+  // console.log(newFriend)
+  // const sendFriend = await update(newFriend)
+  // setMyProfile(sendFriend);
+  //   }
+
+  async function handleAddFriend(friendId) {
+    try {
+      const friendsArray = Array.isArray(myProfile.friends)
+        ? myProfile.friends
+        : [];
+      if (myProfile._id === friendId) {
+        console.error("Cannot add yourself as a friend");
+        return;
+      }
+      if (friendsArray.includes(friendId)) {
+        console.error("Already added as a friend");
+      }
+
+      // Optimistically update the local state
+      // const updatedProfile = {
+      //   ...myProfile,
+      //   friends: [...friendsArray, friendId],
+      // };
+      // setMyProfile(updatedProfile);
+
+      const response = await update({
+        ...myProfile,
+        friends: [...friendsArray, friendId],
+      });
+      setMyProfile(response);
+    } catch (error) {
+      console.error("Error adding friend:", error);
+      // Revert to the original state in case of an error
+      setMyProfile(myProfile);
+    }
   }
-  
   return (
     <div>
       <h1>User List</h1>
@@ -38,7 +69,7 @@ setMyProfile(sendFriend);
               <div className="user-avatar">P</div>
               <strong className="div-text">{p.username}</strong>
             </Link>
-              <button onClick={() => handleAddFriend(p._id)}>Add Friend</button>
+            <button onClick={() => handleAddFriend(p._id)}>Add Friend</button>
           </li>
         ))}
       </ul>
