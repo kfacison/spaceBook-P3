84eba6adc7c5070f6da47eab40bf0df38fd70ea3
mHaines331 <mhaines9219@gmail.com>
2023-11-25 22:30:40 -0800

    edit btn only appears when user id matches url

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import "./AboutMe.css";
 import { Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 export default function AboutMe({ myProfile }) {
+  let { id } = useParams();
+
+  console.log(id);
   return (
     <>
       <div id="profile-photo-container">div</div>
@@ -10,17 +14,24 @@ export default function AboutMe({ myProfile }) {
         <div id="about-me-box">
           <div className="about-me-cat">Name: {myProfile.username}</div>
           <div className="about-me-cat">Species: {myProfile.species}</div>
-          <div className="about-me-cat">Favorite Planet: {myProfile.favPlanet}</div>
+          <div className="about-me-cat">
+            Favorite Planet: {myProfile.favPlanet}
+          </div>
           <div className="about-me-cat">Bio: {myProfile.bio}</div>
         </div>
         {/* if the user have the same id them they can see the edit button {myProfile.user} */}
-        <Link to={"/profiles/"+myProfile.user+"/edit"} id="edit-profile-link">
-          <img
-            src="https://i.ibb.co/ysycYrk/edit-square-FILL0-wght400-GRAD0-opsz24.png"
-            alt="Edit Icon"
-            id="edit-button"
-          />
-        </Link>
+        {id === myProfile._id ? (
+          <Link
+            to={"/profiles/" + myProfile.user + "/edit"}
+            id="edit-profile-link"
+          >
+            <img
+              src="https://i.ibb.co/ysycYrk/edit-square-FILL0-wght400-GRAD0-opsz24.png"
+              alt="Edit Icon"
+              id="edit-button"
+            />
+          </Link>
+        ) : null}
       </div>
     </>
   );
