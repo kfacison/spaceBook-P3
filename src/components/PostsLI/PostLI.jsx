1adf252891cd09b46f09e5114b97f5b182d7c03e
caitlincorrigan <caitlinfcorrigan@gmail.com>
2023-11-28 12:06:13 -0800

    formats date

--- src/components/PostsLI/PostLI.jsx
+++ src/components/PostsLI/PostLI.jsx
@@ -3,11 +3,11 @@ import { React } from "react";
 export default function PostLI({ post, createdAt }) {
   // Display page's posts
   //   const displayPosts = pagePosts.map((p, idx) => (<li key={idx} >{p.content} <br></br> Written by: {p.author}</li>))
-
+  const date = new Date(createdAt)
   return (
     <li>
       {post}
-      <br></br>Written on: {createdAt}
+      <br></br>Written on: {date.toLocaleString('default', { month: 'long', day: 'numeric', year:'numeric' })}
     </li>
   );
 }
